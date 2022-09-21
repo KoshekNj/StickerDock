2ac38ff8377a23475cad9fce2d0b4b4cf65b65d4
KoshekNj <tea.zupanovic@gmail.com>
2022-09-21 15:40:11 +0200

    testing react dnd lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { Canvas } from "./Components/Sticker/Canvas/canvas";
+import { IStickerProps, Sticker } from "./Components/Sticker/sticker";
+
+export const ItemTypes = {
+  STICKER: "sticker",
+};
 
 function App() {
-  return <div className="bg-red-500"></div>;
+  const [stickers, setStickers] = React.useState<IStickerProps[]>([
+    { path: "//picsum.photos/200/200", x: 0, y: 0 },
+  ]);
+
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <Canvas setStickers={setStickers}></Canvas>
+      {stickers.map((sticker) => (
+        <Sticker path={sticker.path} x={sticker.x} y={sticker.y}></Sticker>
+      ))}
+    </DndProvider>
+  );
 }
 
 export default App;
